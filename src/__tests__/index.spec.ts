970c8b4a0ca025115c0a254e8c30245a7c9d78d5
Tantan Fu <luckytantanfu@gmail.com>
2017-11-29 21:25:12 +0800

    add matchBegin matchEnd

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,26 @@
-describe("test", () => {
-  it('should be true', () => {
-    expect(true).toBeTruthy();
+import { RegexBuilder } from '../index';
+
+describe('RegexBuilder', () => {
+  it('should matchBegin', () => {
+    expect(new RegexBuilder().matchBegin().build()).toBe('^');
   });
-})
+
+  it('should matchEnd', () => {
+    expect(new RegexBuilder().matchEnd().build()).toBe('$');
+  });
+
+  it('zeroOrMoreOf', () => {
+    expect(new RegexBuilder().zeroOrMoreOf('a').build()).toBe('a*');
+    expect(new RegexBuilder().zeroOrMoreOf('abc').build()).toBe('(abc)*');
+  });
+
+  it('oneOrMoreOf', () => {
+    expect(new RegexBuilder().oneOrMoreOf('a').build()).toBe('a+');
+    expect(new RegexBuilder().oneOrMoreOf('abc').build()).toBe('(abc)+');
+  });
+
+  it('optional', () => {
+    expect(new RegexBuilder().optional('a').build()).toBe('a?');
+    expect(new RegexBuilder().optional('abc').build()).toBe('(abc)?');
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const sayHello = () => {
-  return 'Hello';
-};
+export class RegexBuilder {
+  private _matchBegin: boolean = false;
+  private _matchEnd: boolean = false;
+  private _regStr: string = '';
+
+  matchBegin = () => { this._matchBegin = true; return this; };
+  matchEnd = () => { this._matchEnd = true; return this; };
+
+  zeroOrMoreOf = (expression: string) => {
+    this._regStr += `${this._addParenthesesIfNeed(expression)}*`;
+    return this;
+  }
+
+  oneOrMoreOf = (expression: string) => {
+    this._regStr += `${this._addParenthesesIfNeed(expression)}+`;
+    return this;
+  }
+
+  optional = (expression: string) => {
+    this._regStr += `${this._addParenthesesIfNeed(expression)}?`;
+    return this;
+  }
+
+  aSingleCharacter = () => {
+    this._regStr += '.';
+    return this;
+  }
+
+  build = () => {
+    if (this._matchBegin) { this._regStr = '^' + this._regStr; }
+    if (this._matchEnd) { this._regStr = this._regStr + '$'; }
+    return this._regStr;
+  }
+
+  private _addParenthesesIfNeed = (expression: string): string => {
+    return expression.length > 1 ? `(${expression})` : expression;
+  }
+}
